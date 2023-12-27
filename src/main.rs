4bd8ccadf73734dff0605715b37e109f559e7445
AxelTahmid <axel.tahmid@gmail.com>
2023-12-27 21:28:31 +0600

    chore: update lookup flow

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,50 @@
+use email_address::EmailAddress;
 use tokio::io::AsyncReadExt;
 use tokio::io::AsyncWriteExt;
 use tokio::net::TcpStream;
 use tokio::time::Duration;
 use trust_dns_resolver::TokioAsyncResolver;
 
-async fn validate_email(email: &str) -> Result<(), Box<dyn std::error::Error>> {
-    // Step 1: Extract domain from the email address
-    let domain: Vec<&str> = email.split('@').collect();
-    let domain = domain.get(1).ok_or("Invalid email format")?;
+#[tokio::main]
+async fn main() {
+    let email_address = "test@example.com";
 
-    // Step 2: Check DNS MX records
+    match validate_email(email_address).await {
+        Ok(_) => println!("Email address is valid"),
+        Err(err) => eprintln!("Validation failed: {}", err),
+    }
+}
+
+async fn validate_email(email: &str) -> Result<(), EmailVerificationError> {
+    // Step 1: Syntax validation using email_address
+    let parsed_email = EmailAddress::from_str(email)?;
+
+    // Step 2: MX record lookup (unchanged)
     let resolver = TokioAsyncResolver::tokio_from_system_conf()?;
-    let mx_records = resolver.mx_lookup(domain).await?;
+    let mx_records = resolver.mx_lookup(parsed_email.domain()).await?;
     if mx_records.is_empty() {
-        return Err("No MX records found for the domain".into());
+        return Err(EmailVerificationError::NoMxRecords);
     }
 
-    // Step 3: Establish an SMTP connection
+  // Step 3: SMTP connection (unchanged)
     let smtp_server = &format!("{}:25", mx_records[0].exchange());
-    let smtp_connection = TcpStream::connect(smtp_server).await?;
+    let smtp_connection: TcpStream = TcpStream::connect(smtp_server).await?;
+
 
     // Step 4: Send SMTP commands
-    send_smtp_commands(smtp_connection, email).await?;
+    let commands = [
+        &format!("EHLO {}", parsed_email.local_part()), // Use recipient's local part for EHLO
+        "MAIL FROM: <sender@example.com>\r\n",
+        &format!("RCPT TO: <{}>\r\n", email),
+    ];
+    send_smtp_commands(smtp_connection, commands).await?;
 
     Ok(())
 }
 
-async fn send_smtp_commands(mut stream: TcpStream, email: &str) -> Result<(), Box<dyn std::error::Error>> {
-    let commands = [
-        "EHLO example.com\r\n",
-        "MAIL FROM: <sender@example.com>\r\n",
-        &format!("RCPT TO: <{}>\r\n", email),
-        "DATA\r\n",
-        "Subject: Test email\r\n",
-        "This is a test email.\r\n",
-        ".\r\n",
-    ];
-
-    for command in &commands {
+async fn send_smtp_commands(mut stream: TcpStream, commands: &str) -> Result<(), Box<dyn std::error::Error>> {
+    
+     for command in &commands {
         tokio::time::sleep(Duration::from_millis(100)).await; // Delay to avoid rate limiting
         stream.write_all(command.as_bytes()).await?;
 
@@ -55,12 +62,24 @@ async fn send_smtp_commands(mut stream: TcpStream, email: &str) -> Result<(), Bo
     Ok(())
 }
 
-#[tokio::main]
-async fn main() {
-    let email_address = "test@example.com";
 
-    match validate_email(email_address).await {
-        Ok(_) => println!("Email address is valid"),
-        Err(err) => eprintln!("Validation failed: {}", err),
+#[derive(Debug)]
+enum EmailVerificationError {
+    InvalidEmailFormat,
+    NoMxRecords,
+    SmtpConnectionError(Box<dyn std::error::Error>),
+    SmtpCommandError(String),
+}
+
+impl std::fmt::Display for EmailVerificationError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::InvalidEmailFormat => f.write_str("Invalid email format"),
+            Self::NoMxRecords => f.write_str("No MX records found for the domain"),
+            Self::SmtpConnectionError(err) => f.write_fmt(format_args!("SMTP connection error: {}", err)),
+            Self::SmtpCommandError(err) => f.write_fmt(format_args!("SMTP command error: {}", err)),
+        }
     }
 }
+
+impl std::error::Error for EmailVerificationError {}
